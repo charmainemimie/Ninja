81c3cceef3e948a32f36d614183ad413aefc045c
charmiainemimie <charmainemangorima@gmail.com>
2024-05-06 09:42:26 +0200

    tailwind and vite setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center  bg-orange-400 min-h-screen">
+      <div className=" pb-10">
+        <h1 className="text-3xl ">Ninjago - a Ninja Rest API</h1>
+      </div>
+
+      <div>
+        <input type="text" name="" id="" className="w-96 h-16 px-4" placeholder="Hire a ninja in your area"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
